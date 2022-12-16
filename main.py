dbfe35a2aff2381ef535d3a1c3e4b1ee8d53ab7c
Smarandii <olegsmarandi@gmail.com>
2022-12-16 07:23:20 +0300

    reformating & renaming

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from PIL import Image
 from yoba_automization import yo
 
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QSize, QPoint
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QWidget, QMessageBox
+from PyQt5.QtCore import QPoint
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 import sys
 import os
 from loguru import logger
+
 logger.add('debug.log', format='{time} {level} {message}', level='DEBUG', rotation='10 KB', compression='zip')
 
 
@@ -25,8 +26,11 @@ class SizeDirectory:
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.initUI()
+        self.working_directory = None
+        self._init_ui()
         self.default_render_attributes = {'extension': 'mp4', 'duration': 5, 'fade': False}
+        self.stats = {'digitals': 0,
+                      'prints': 0}
 
     def _add_label(self, label_name: str, text: str = None, point: QPoint = None):
         setattr(self, label_name, QtWidgets.QLabel(self))
@@ -36,7 +40,17 @@ class MainWindow(QMainWindow):
         if point:
             self.__getattribute__(label_name).move(point)
 
-    def initUI(self):
+    def _add_button(self, button_name: str, text: str = None, point: QPoint = None, on_click=None):
+        setattr(self, button_name, QtWidgets.QPushButton(self))
+        if text:
+            self.__getattribute__(button_name).setText(text)
+            self.__getattribute__(button_name).adjustSize()
+        if point:
+            self.__getattribute__(button_name).move(point)
+        if on_click:
+            self.__getattribute__(button_name).clicked.connect(on_click)
+
+    def _init_ui(self):
         self._add_label(label_name='label_about_path',
                         text='Путь к папке с макетами:',
                         point=QPoint(10, 10))
@@ -52,25 +66,21 @@ class MainWindow(QMainWindow):
                         point=QPoint(150, 60))
 
         self.path_window = QMainWindow()
-        self.choose_path_btn = QtWidgets.QPushButton(self)
-        self.choose_path_btn.setText('Выбрать директорию')
-        self.choose_path_btn.adjustSize()
-        self.choose_path_btn.move(10, 30)
-        self.choose_path_btn.clicked.connect(self.choose_path)
+        self._add_button(button_name='choose_path_btn',
+                         text='Выбрать директорию',
+                         point=QPoint(10, 30),
+                         on_click=self.choose_path)
+        self._add_button(button_name='render_digital_btn',
+                         text='Рендер digital',
+                         point=QPoint(350, 30),
+                         on_click=self.render_digital)
 
-        self.render_digital_btn = QtWidgets.QPushButton(self)
-        self.render_digital_btn.setText('Рендер digital')
-        self.render_digital_btn.adjustSize()
-        self.render_digital_btn.move(350, 30)
-        self.render_digital_btn.clicked.connect(self.render_digital)
+        self._add_button(button_name='complex_render_digital_btn',
+                         text='Рендер + Каталогизация digital',
+                         point=QPoint(350, 55),
+                         on_click=self.complex_digital_render)
 
-        self.complex_render_digital_btn = QtWidgets.QPushButton(self)
-        self.complex_render_digital_btn.setText('Рендер + Каталогизация digital')
-        self.complex_render_digital_btn.adjustSize()
-        self.complex_render_digital_btn.move(350, 55)
-        self.complex_render_digital_btn.clicked.connect(self.complex_digital_render)
-
-    def create_directory(self, file_name):
+    def create_directory(self, file_name) -> SizeDirectory:
         if ".jpg" in file_name or '.ai' in file_name:
             size_dir_path = os.path.join(self.working_directory, (file_name.replace('.jpg', '')).replace('.ai', ''))
             mp4_dir_path = os.path.join(size_dir_path, "Видео")
@@ -83,11 +93,11 @@ class MainWindow(QMainWindow):
                 os.mkdir(jpg_dir_path)
                 os.mkdir(ai_dir_path)
             except FileExistsError:
-               pass
+                pass
             return SizeDirectory(jpg_dir_path, mp4_dir_path, ai_dir_path, size_dir_path)
 
     @staticmethod
-    def check_size(size: tuple):
+    def check_size(size: tuple) -> bool:
         return size[0] % 2 == 0 and size[1] % 2 == 0
 
     def check_pixels(self, file_path, file_name):
@@ -95,7 +105,8 @@ class MainWindow(QMainWindow):
         if self.check_size(img_file.size) is False:
             warning = QMessageBox()
             warning.setIcon(QMessageBox.Critical)
-            warning.setText(f"{file_name} кажется в .jpg файле есть лишние пиксели!\n(Или в разрешении есть нечетное число)")
+            warning.setText(f"{file_name} кажется в .jpg файле есть лишние пиксели!\n"
+                            f"(Или в разрешении есть нечетное число)")
             warning.setInformativeText(f"Путь к файлу: {file_path}")
             warning.setWindowTitle(f"Лишние пиксели в {file_name}!")
             warning.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
@@ -111,18 +122,19 @@ class MainWindow(QMainWindow):
                     shutil.move(file_path, size_dir.jpg_dir_path)
                     render_attributes = self.parse_file_name(str(file_path))
                     input_file_path = os.path.join(size_dir.jpg_dir_path, file_name)
-                    size = yo.Size(input_file_path=input_file_path,
-                                   video_extension=render_attributes['extension'],
-                                   video_duration=render_attributes['duration'])
+                    size = yo.FfmpegGenerator(input_file_path=input_file_path,
+                                              video_extension=render_attributes['extension'],
+                                              video_duration=render_attributes['duration'])
                     try:
                         size.create_preview()
                         size.render_video()
-                    except:
+                    except [FileExistsError, FileNotFoundError]:
                         pass
                 if '.ai' in file_name:
                     shutil.move(file_path, size_dir.ai_dir_path)
 
-    def parse_file_name(self, name: str):
+    @staticmethod
+    def parse_file_name(name: str) -> dict:
         render_attributes = {'extension': 'mp4', 'duration': 5, 'fade': False}
         if "avi" in name.lower():
             render_attributes['extension'] = 'avi'
@@ -138,18 +150,18 @@ class MainWindow(QMainWindow):
                 jpg_path = os.path.join(pathlib.Path(root), files[0])
                 self.check_pixels(jpg_path, files[0])
                 render_attributes = self.parse_file_name(str(jpg_path))
-                mp4_path = os.path.join(pathlib.Path(root).parent, "Видео", files[0].replace('jpg', 'mp4'))
-                size = yo.Size(input_file_path=jpg_path,
-                        video_extension=render_attributes['extension'],
-                        video_duration=render_attributes['duration'])
+                size = yo.FfmpegGenerator(input_file_path=jpg_path,
+                                          video_extension=render_attributes['extension'],
+                                          video_duration=render_attributes['duration'])
                 try:
                     size.create_preview()
                     size.render_video()
-                except:
+                except [FileExistsError, FileNotFoundError]:
                     pass
 
     def choose_path(self):
-        self.working_directory = str(QFileDialog.getExistingDirectory(self.path_window, "Выберите директорию с размерами"))
+        self.working_directory = str(
+            QFileDialog.getExistingDirectory(self.path_window, "Выберите директорию с размерами"))
         self.label_path.setText(self.working_directory)
         self.label_path.adjustSize()
         self.calculate_statistic()
@@ -161,14 +173,12 @@ class MainWindow(QMainWindow):
         self.label_about_digital.adjustSize()
 
     def calculate_statistic(self):
-        self.stats = {'digitals': 0,
-                 'prints': 0}
 
         for root, dirs, files in os.walk(self.working_directory):
-            for dir in dirs:
-                if dir.upper() == "JPG":
+            for directory in dirs:
+                if directory.upper() == "JPG":
                     self.stats['digitals'] += 1
-                if dir.capitalize() == "Принт":
+                if directory.capitalize() == "Принт":
                     self.stats['prints'] += 1
             if self.stats['digitals'] == 0:
                 for file in files:
--- yoba_automization/yo.py
+++ yoba_automization/yo.py
@@ -8,7 +8,7 @@ from loguru import logger
 logger.add('debug.log', format='{time} {level} {message}', level='DEBUG', rotation='10 KB', compression='zip')
 
 
-class Size:
+class FfmpegGenerator:
     def __init__(self, video_extension, video_duration, input_file_path):
 
         self.jpg_path = pathlib.Path(input_file_path)
@@ -41,15 +41,19 @@ class Size:
         self.delete_old_preview(self.DIR_PATH.parent)
         shutil.copy(self.jpg_path, self.DIR_PATH.parent)
 
-        logger.debug(f"RENAME: {os.path.join(self.DIR_PATH.parent, self.name).__str__()} to the {os.path.join(self.DIR_PATH.parent, self.name_with_preview)}")
-        os.rename(os.path.join(self.DIR_PATH.parent, self.name), os.path.join(self.DIR_PATH.parent, self.name_with_preview))
+        logger.debug(f"RENAME: {os.path.join(self.DIR_PATH.parent, self.name).__str__()} to the "
+                     f"{os.path.join(self.DIR_PATH.parent, self.name_with_preview)}")
+        os.rename(os.path.join(self.DIR_PATH.parent, self.name),
+                  os.path.join(self.DIR_PATH.parent, self.name_with_preview))
 
         logger.debug(f"COPY: {self.jpg_path} to the {self.DIR_PATH.parent.parent.__str__()}")
         self.delete_old_preview(self.DIR_PATH.parent.parent)
         shutil.copy(self.jpg_path, self.DIR_PATH.parent.parent)
 
-        logger.debug(f"RENAME: {os.path.join(self.DIR_PATH.parent.parent, self.name).__str__()} to the {os.path.join(self.DIR_PATH.parent.parent, self.name_with_preview)}")
-        os.rename(os.path.join(self.DIR_PATH.parent.parent, self.name), os.path.join(self.DIR_PATH.parent.parent, self.name_with_preview))
+        logger.debug(f"RENAME: {os.path.join(self.DIR_PATH.parent.parent, self.name).__str__()} to the "
+                     f"{os.path.join(self.DIR_PATH.parent.parent, self.name_with_preview)}")
+        os.rename(os.path.join(self.DIR_PATH.parent.parent, self.name),
+                  os.path.join(self.DIR_PATH.parent.parent, self.name_with_preview))
 
     def render_video(self):
         # Old way to use ffmpeg:
@@ -57,17 +61,18 @@ class Size:
         # -pix_fmt yuv420p "{self.video_path}""')
 
         logger.debug(f"Executing...\n"
-              f"ffmpeg \n"
-              f"-y -loop 1 \n"
-              f"-i {self.jpg_path} \n"
-              f"-t {self.video_duration} \n"
-              f"-pix_fmt yuv420p {self.video_path} \n")
-        subprocess.call(f"ffmpeg -y -loop 1 -i \"{self.jpg_path}\" -t {self.video_duration} -pix_fmt yuv420p \"{self.video_path}\"", shell=True)
+                     f"ffmpeg \n"
+                     f"-y -loop 1 \n"
+                     f"-i {self.jpg_path} \n"
+                     f"-t {self.video_duration} \n"
+                     f"-pix_fmt yuv420p {self.video_path} \n")
+        subprocess.call(
+            f"ffmpeg -y -loop 1 -i \"{self.jpg_path}\" -t {self.video_duration} -pix_fmt yuv420p \"{self.video_path}\"")
         if self.fade_in_fade_out:
             f_in_fname = self.video_path.replace('.mp4', '_fade_in.mp4')
             f_in_f_out_fname = f_in_fname.replace('_fade_in.mp4', '_fade_in_fade_out.mp4')
-            subprocess.call(f"ffmpeg -y -i \"{self.video_path}\" -vf \"fade=t=in:st=0:d=1\" \"{f_in_fname}\"", shell=True)
-            subprocess.call(f"ffmpeg -y -i \"{f_in_fname}\" -vf \"fade=t=out:st=4:d=1\" \"{f_in_f_out_fname}\"", shell=True)
+            subprocess.call(f"ffmpeg -y -i \"{self.video_path}\" -vf \"fade=t=in:st=0:d=1\" \"{f_in_fname}\"")
+            subprocess.call(f"ffmpeg -y -i \"{f_in_fname}\" -vf \"fade=t=out:st=4:d=1\" \"{f_in_f_out_fname}\"")
             os.remove(f_in_fname)
 
 
@@ -95,7 +100,7 @@ if __name__ == "__main__":
         args.e = "mp4"
     if args.t is None:
         args.t = "5"
-    size = Size(video_extension=args.e, video_duration=args.t, input_file_path=args.i)
+    size = FfmpegGenerator(video_extension=args.e, video_duration=args.t, input_file_path=args.i)
 
     try:
         size.create_preview()
@@ -106,4 +111,3 @@ if __name__ == "__main__":
         size.render_video()
     except Exception as e:
         print(e)
-
